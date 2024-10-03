0f8ba88939671cb12d9db34a6b9c1ab9a14f79c2
Seyoung Hong <maiorem00@gmail.com>
2024-10-04 03:36:50 +0900

    feature: 단위테스트

--- src/main/java/com/hhplus/api/domain/lecture/domain/Lecture.java
+++ src/main/java/com/hhplus/api/domain/lecture/domain/Lecture.java
@@ -1,13 +1,19 @@
 package com.hhplus.api.domain.lecture.domain;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.Comment;
 
 @Getter
 @Entity
+@Builder
 @Table(name = "TB_LECTURE")
 @Comment("특강 테이블")
+@NoArgsConstructor
+@AllArgsConstructor
 public class Lecture {
 
     @Id
